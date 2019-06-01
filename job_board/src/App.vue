097107ddf972d35b63f6b92a32bee741fb4a16f1
Lauren <laurenqurashi@me.com>
2019-06-01 16:14:11 +0100

    Initial project and tests set up. Mocha and Chai for Unit testing, and Cypress for e2e testing. One initial test each, both passing.

---
+++ job_board/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <!-- How the page will look -->
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Hello Lauren!"/>
+  </div>
+</template>
+
+<script>
+// Data from the page.
+import HelloWorld from './components/HelloWorld.vue';
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld,
+  },
+};
+</script>
+
+<style>
+/*  Includes the styling for the root component*/
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
